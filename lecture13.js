6a1881841b75c0a3d837225e50a35055660ea093
sohyun-alicia <78770235+sohyun-alicia@users.noreply.github.com>
2021-03-17 17:37:25 +0900

    string manipulation

--- lecture13.js
+++ lecture13.js
@@ -1,28 +1,70 @@
-// Console
+//-------------------- 문자열 이어 붙이기 : str1.concat(str2)
 
-// var arr=[1, 2, 3, 4, 5];
+// var str = "Hello";
+// str.length;
+// var str2 = "World";
+// var str3 = str.concat(str2)     // 문자열 이어붙여주기
+
+// 실습(콘솔)
+
+// var prefix = "program";
 // undefined
-// arr.pop();
-// 5
-// arr;
-// (4) [1, 2, 3, 4]
-// arr.shift();
+// var suffix = "mer";
+// undefined
+// var word;
+// undefined
+// var word_length;
+// undefined
+// var word = prefix.concat(suffix);
+// undefined
+// word;
+// "programmer"
+// (anonymous) @ VM651:1
+// word.length;
+// 10
+// word_length
+// undefined
+
+
+//--------------------------문자열 다루기----------------------//
+
+// 인덱싱 : charAt()
+// var str = 'abcdeabced';
+// undefined
+// str.charAt(0);        인덱스 넘버에 해당하는 값 호출
+// "a"
+// str.length;           글자 길이
+// 10
+// str.charAt(9);
+// "d"            
+// str.charAt(str.length-2);            끝에서부터 두번째 값 가져오기
+// "e"
+
+// 슬라이싱 : substring, substr
+// str.substring(2,4);                  2번 인덱스부터 4번 인덱스 값 전까지 호출
+// "cd"
+// str.substr(2,4);                     2번 인덱스부터 4개 값 호출
+// "cdea"
+// str.substring(2);                    2번 인덱스 값부터 끝까지
+// "cdeabced"
+// str.substr(2);                       2번 인덱스 값부터 끝까지
+// "cdeabced"
+// str.substr(-7);                      끝에서부터 끝에서 7번째 값까지
+// "deabced"
+// str.substr(-7,2);                    끝에서 7번째부터 두개 값
+// "de"
+// str.indexOf("bc");                   호출하는 값이 처음 나오는 인덱스 값
 // 1
-// arr;
-// (3) [2, 3, 4]
-// arr.push(6);
-// 4
-// arr.unshift(0);
-// 5
-// arr;
-// (5) [0, 2, 3, 4, 6]
+// str.lastIndexOf("bc");               호출하는 값이 마지막으로 나오는 인덱스 값
+// 6
+// str.lastIndexOf("f");                없는 값을 호출할 경우 -1 반환
+// -1
 
+//실습
+function firstLetter(str){
+    return str.charAt(0)
+}
 
-
-// var name = "홍길동"; 
-//개발할 때는 사용자 입력 대신 변수를 만들어 사용하면 편리함
-var name = prompt("이름을 입력해 주세요.");
-var msg = "당신의 이름은" + name + "입니다." + name + "님의 이름은" + name.length + "글자 입니다."
-// console.log(msg); //개발할 때는 경고창을 띄우지 말고 콘솔에서 메시지를 확인
-
-alert(msg);
+function first2Letters(str){
+    return str.substring(0,2)
+}
