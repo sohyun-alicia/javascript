b6ea5241b1dd85cc7b555c0a8f2186b6091e5f9e
sohyun-alicia <78770235+sohyun-alicia@users.noreply.github.com>
2021-03-17 17:37:01 +0900

    operator

--- lecture12.js
+++ lecture12.js
@@ -1,6 +1,58 @@
-var str = "Hello";
-str.length;
+//-----------------------연산자-------------------------//
 
-var str2 = "World";
+var e = "a", f = "b", g = "c", h = "d";
 
-var str3 = str.concat(str2)     // 문자열 이어붙여주기
+// 콘솔
+// e == e;
+// true
+// e != e;
+// false
+// e < f;
+// true
+// f < e;
+// false
+
+// true && true;        && and : 둘다 true여아 결과값 true
+// true
+// true && false;
+// false
+// false && false;
+// false
+// true || true;
+// true
+// true || false;       || or : 둘중 하나만 true여도 결과값 true
+// true
+// false || false;
+// false
+
+// not(!)
+// !true
+// false
+// !false
+// true
+
+// var height = 180;
+// undefined
+// var age = 20;
+// undefined
+// height >= 180 && age >= 20 && age <30
+// true
+// height = 170;
+// 170
+// height >= 180 && age >= 20 && age <30
+// false
+// age < 8 || age >= 65;
+// false
+// age = 70;
+// 70
+// age < 8 || age >= 65;
+// true
+
+// exercise
+function isBigger(a,b){
+    return a > b;
+}
+
+function isBothTrue(a,b){
+    return a && b;
+}
