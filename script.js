955e8bfc0579f55036361431bdac39fcdfe15a87
sohyun-alicia <78770235+sohyun-alicia@users.noreply.github.com>
2021-03-25 17:39:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
-function eventHandler() {
-    console.log("eventHandler");
-    function innerFunction(){
-        console.log("innerFunction");
-    }
-    for (var i=0;i<2;i++) {
-        innerFunction();
-    }
+//------------------------익명 함수 ---------------------//
+// var sum = 0;
+
+// // 1~50까지 합 출력
+// for (var i=1; i<=50; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+// // 1~5까지 값 출력
+// (function (){
+//     for (var i=1; i<=5; i++){
+//         console.log(i);
+//     }
+// })();
+
+// // 51~100까지 합 출력
+// for (;i<=100; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+
+//------------------------settime과 callback활용 익명함수---------------------//
+// 1. 
+// function callback(){
+//     console.log("timeout");
+// }
+
+// setTimeout(callback(), 3000);
+
+
+// 2. 
+// setTimeout(function callback(){
+//     console.log("timeout");
+// }, 3000);
+
+// 3. 
+// setTimeout(function (){
+//     console.log("timeout");
+// }, 3000);
+
+
+try{
+    console.log("try-1");
+    throw "error";
+    console.log("try-2");
 }
+
+catch(e){
+    console.log("catch error:", e);
+}
+
+finally{
+    console.log("finally-this code will always be executed")
+}
+
